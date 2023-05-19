823222946bfd898efe98edbc8ac444ce00cb2f8c
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-19 16:07:08 -0300

    Initialize repository

---
+++ clase-1/primerClase.py
@@ -0,0 +1,6 @@
+# Escribir un programa que permita al usuario ingresar su nombre.
+# El programa debe emitir una salida con un mensaje de bienvenida que 
+# incluya el nombre ingresado.
+
+nombre = input("Ingrese su nombre: ")
+print("Bienvenido", nombre) 
