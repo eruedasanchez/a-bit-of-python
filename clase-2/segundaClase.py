e72ec20f9ae14b910c8043ab710b4d9b8131a0cf
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-19 18:11:05 -0300

    Add variables declaration and first addition algorithm

---
+++ clase-2/segundaClase.py
@@ -0,0 +1,24 @@
+# Segunda clase del curso de Python
+
+# Asignacion de variables
+
+f = 0
+s = 0 
+x = "pepe"
+uuu = 2.789545
+f = "xxxxx"     # f es un tipo de dato dinamico
+f = 0           # f se vuelve a resignar 
+
+# Ejercicio 1. Leer dos numeros y mostrar la suma
+
+primer_numero = float(input("Ingrese el primer numero a sumar: "))
+segundo_numero = int(input("Ahora, ingrese el segundo numero a sumar: ")) 
+suma = primer_numero + segundo_numero
+print("El resultado de la suma entre", primer_numero, "y", segundo_numero,  "es igual a", suma)
+
+division = suma / 5
+print("El resultado de dividir", suma, "sobre 5 es igual a", division) 
+
+# Por defecto, suma de un numero flotante y un numero entero es igual a float y dividir por un numero es igual a float por defecto 
+
+
